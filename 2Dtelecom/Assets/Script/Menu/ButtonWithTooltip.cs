cef7131c2b36bb28a257ec19d0e103faf58d7316
Glosur <s.blanc@hotmail.fr>
2015-09-23 22:21:16 +0200

    Sous Menu dynamique
    Ajout du sous  menu options

--- 2Dtelecom/Assets/Script/Menu/ButtonWithTooltip.cs
+++ 2Dtelecom/Assets/Script/Menu/ButtonWithTooltip.cs
@@ -5,37 +5,53 @@ public class ButtonWithTooltip : MonoBehaviour {
 
 
     public Rect rectangle; //Rectangle
-    private string textToDisplay; // Variable stockant le texte a afficher, modifier par la fct animString
-    public string textTooltipFinal; // Texte du Tooltip
-    public string textButton; // Texte du Button
 
+    private string textTooltipToDisplay; // Variable stockant le texte a afficher, modifier par la fct animString
+    private string textButtonToDisplay;
+    public string textTooltipFinal; // Texte du Tooltip
+    public string textButtonFinal; // Texte du Button
+
+
+    private bool isClicked;
+    private bool displayed;
     private char lastChar; // dernier charac stocke pour random pas trop rapide
 
     private int dureeAffichage;
-
-    private int state; // Etat de l'ecriture du striong allant dfe 1 a dureeAffich
+    private bool displayingAnim;
+    private int stateTooltip; // Etat de l'ecriture du striong allant dfe 1 a dureeAffich
+    private int stateAnimatedDisplay;
 
     private GUISkin skin1;
 
     public ButtonWithTooltip(string textButt, string textTooltip, Rect rectangleParam, int dureeAffichageParam = 1500)
     {
-        textButton = textButt;
+        textButtonFinal = textButt;
         textTooltipFinal = textTooltip;
         rectangle = rectangleParam;
         dureeAffichage = dureeAffichageParam;
-        textToDisplay = "";
-        state = 0;
+        textTooltipToDisplay = "";
+        textButtonToDisplay = textButtonFinal;
+        stateTooltip = 0;
+        stateAnimatedDisplay = 0;
+        isClicked = false;
+        displayed = true;
+        displayingAnim = false;
     }
 
     public void init(string textButt, string textTooltip, Rect rectangleParam, int dureeAffichageParam = 1500)
     {
-        textButton = textButt;
+        textButtonFinal = textButt;
         textTooltipFinal = textTooltip;
         rectangle = rectangleParam;
         dureeAffichage = dureeAffichageParam;
-        textToDisplay = "";
-        state = 0;
-        
+        textTooltipToDisplay = "";
+        textButtonToDisplay = textButtonFinal;
+        stateTooltip = 0;
+        stateAnimatedDisplay = 0;
+        isClicked = false;
+        displayed = true;
+        displayingAnim = false;
+
     }
 
 
@@ -48,40 +64,63 @@ public class ButtonWithTooltip : MonoBehaviour {
 	void Update () {
         Vector2 mousePos;
         mousePos = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+
+        // If d'affichage du tooltip
         if (mousePos.x > rectangle.x && mousePos.x < (rectangle.x + rectangle.width) && mousePos.y > rectangle.y && mousePos.y < (rectangle.y + rectangle.height))
         {
-            if (state < dureeAffichage)
+            if (stateTooltip < dureeAffichage)
             {
-                state+=10;
-                animString();
+                stateTooltip+=10;
+                animStringTooltip();
             }
         }
         else
         {
-            state = 0;
-            textToDisplay = textTooltipFinal;
+            stateTooltip = 0;
+            textTooltipToDisplay = textTooltipFinal;
 
         }
        
+        if(displayingAnim)
+        {
+            if (stateAnimatedDisplay < dureeAffichage)
+            {
+                stateAnimatedDisplay += 10;
+                animatedDisplay();
+            }
+            else
+                displayingAnim = false;
+        }
+        else
+        {
+            stateAnimatedDisplay = 0;
+            textButtonToDisplay = textButtonFinal;
+        }
+
 
     }
 
     void OnGUI()
     {
-        GUI.skin = skin1;
-        // Genere le button, le tooltip est con,fig par le label situe juste apres
-        GUI.Button(rectangle, new GUIContent(textButton, textToDisplay));
-        GUI.Label(new Rect(Input.mousePosition.x + 10, Screen.height - Input.mousePosition.y + 20, 200, 40), GUI.tooltip);
-
+        if (displayed)
+        {
+            GUI.skin = skin1;
+            // Genere le button, le tooltip est con,fig par le label situe juste apres
+            if (GUI.Button(rectangle, new GUIContent(textButtonToDisplay, textTooltipToDisplay)))
+            {
+                isClicked = !isClicked;
+            }
+            GUI.Label(new Rect(Input.mousePosition.x + 10, Screen.height - Input.mousePosition.y + 20, 200, 40), GUI.tooltip);
+        }
       
     }
 
-    private string animString()
+    private string animStringTooltip()
     {
         int tailleTotale = textTooltipFinal.Length;
         string stringRetour = "";
         int k = 0;
-        for (int i = 0; i < state; i += (dureeAffichage / tailleTotale) + 1)
+        for (int i = 0; i < stateTooltip; i += (dureeAffichage / tailleTotale) + 1)
         {
             stringRetour += textTooltipFinal[k];
             k++;
@@ -90,13 +129,41 @@ public class ButtonWithTooltip : MonoBehaviour {
         //Random rnd = new Random();
         if (!(stringRetour.Length == tailleTotale))
         {
-            if (state % 6 == 0)
+            if (stateTooltip % 6 == 0)
                 lastChar = (char)Random.Range(97, 124);
 
             stringRetour += lastChar;
             stringRetour += '█';
         }
-        textToDisplay = stringRetour;
+        textTooltipToDisplay = stringRetour;
         return stringRetour;
     }
+
+    public void animatedDisplay()
+    {
+        int tailleTotale = textButtonFinal.Length;
+        string stringRetour = "";
+        int k = 0;
+        for (int i = 0; i < stateAnimatedDisplay; i += (dureeAffichage / tailleTotale) + 1)
+        {
+            stringRetour += textButtonFinal[k];
+            k++;
+        }
+        int tailleAvantRand = stringRetour.Length;
+        //Random rnd = new Random();
+        if (!(stringRetour.Length == tailleTotale))
+        {
+            if (stateAnimatedDisplay % 6 == 0)
+                lastChar = (char)Random.Range(97, 124);
+
+            stringRetour += lastChar;
+            stringRetour += '█';
+        }
+        textButtonToDisplay = stringRetour;
+    }
+
+    public bool getIsClicked() { return isClicked; }
+    public void setDisplayed(bool d) { displayed = d; }
+    public void setDisplayingAnim(bool d) { displayingAnim = d; }
+
 }
--- 2Dtelecom/Assets/Script/Menu/Menu.cs
+++ 2Dtelecom/Assets/Script/Menu/Menu.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class Menu : MonoBehaviour
 {
-    private Rect rectBox;
+    private Rect rectMenu;
 
     private ButtonWithTooltip boutonJouer;
     private ButtonWithTooltip boutonOptions;
     private ButtonWithTooltip boutonQuitter;
+
+
+    private ButtonWithTooltip boutonOptionsGraphiques;
+    private ButtonWithTooltip boutonOptionsAudios;
 
     private GUISkin skin1; // GUI skin
 
@@ -18,11 +22,11 @@ public class Menu : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        // init de la taille de ml'image
-        imageFond = new Vector2(Screen.width, Screen.height);
+        skin1 = Resources.Load("Menu/Menu") as GUISkin;
+        
 
         // rect De la fenetre principale, poura fficher l'image de fond et le texte principal
-        rectBox = new Rect(Screen.width / 2 - imageFond.x / 2, Screen.height / 2 - imageFond.y / 2, imageFond.x, imageFond.y);
+        rectMenu = new Rect(4 * Screen.width / 5 , 2* Screen.height / 3, 400, 100);
 
         //rect du 1er bouton
         boutonJouer = gameObject.AddComponent<ButtonWithTooltip>();
@@ -33,13 +37,35 @@ public class Menu : MonoBehaviour
 
         boutonQuitter = gameObject.AddComponent<ButtonWithTooltip>();
         boutonQuitter.init("> Quitter", "Retour sur le bureau", new Rect(50, 3 * Screen.height / 4, 200, 50), 1500);
-        //skin1 = Resources.Load("Menu/Menu") as GUISkin;
+
+        /*
+        *   Sous menu options :
+        */
+        boutonOptionsGraphiques = gameObject.AddComponent<ButtonWithTooltip>();
+        boutonOptionsGraphiques.init("> Options graphiques", "No tooltuip", new Rect(50 + Screen.width/3, Screen.height / 4, 400, 50), 1500);
+        boutonOptionsGraphiques.setDisplayed(true);
+
+        boutonOptionsAudios = gameObject.AddComponent<ButtonWithTooltip>();
+        boutonOptionsAudios.init("> Options audios", "No tooltuip", new Rect(50 + Screen.width / 3, Screen.height / 3, 400, 50), 1500);
+        boutonOptionsAudios.setDisplayed(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(boutonOptions.getIsClicked())
+        {
+            boutonOptionsAudios.setDisplayed(true);
+            boutonOptionsAudios.setDisplayingAnim(true);
+            boutonOptionsGraphiques.setDisplayed(true);
+            boutonOptionsGraphiques.setDisplayingAnim(true);
 
+        }
+        else
+        {
+            boutonOptionsAudios.setDisplayed(false);
+            boutonOptionsGraphiques.setDisplayed(false);
+        }
 
         // Button1 : if souris dessus, afficher tooltip; snon, pas afficher, et remettre compteur a 0
        
@@ -50,7 +76,7 @@ public class Menu : MonoBehaviour
     {
         GUI.skin = skin1;
         // Box prionc + ecran
-         GUI.Box(rectBox, "MENU");
+         GUI.Box(rectMenu, "MENU");
         
     }
 
